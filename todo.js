f34c4901afe1a0dd24c0b60e18a27347b86b49fa
Melissa Uchem <51384282+muchem@users.noreply.github.com>
2019-07-13 20:09:28 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,12 +2,12 @@
 function submit(){
 	var inputvalue = document.getElementById("inputbar").value;
 	var item = document.createElement("li");
-	item.innerHTML = inputvalue;
+		item.innerHTML = inputvalue;
 	document.getElementById("ul").appendChild(item);//append mean attach//li element must be nested under ul element
 	var button = document.createElement("button");
-	item.appendChild(button);
-	button.innerHTML="Clear";
-	button.id ="butt";
+		item.appendChild(button);
+		button.innerHTML="Clear";
+		button.id ="butt";
 	button.onclick = function clear(){
 		item.removeChild(button);
 		document.getElementById("ul").removeChild(item);
